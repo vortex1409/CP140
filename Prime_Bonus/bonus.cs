43ebcaa3c2f697bcd6844969c14be51fa620d25d
Michael <mvd1409@gmail.com>
2016-11-15 15:14:45 -0500

    Create bonus.cs

---
+++ Prime_Bonus/bonus.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+    Project: Prime - Bonus Assignment (Q8 & Q9)
+    Description: Checks to see if number is prime and then prints then adds number to a listbox
+    Class: CP140 - OOP1
+    Name: Michael Dorfman
+    Date: 11/15/2016
+*/
+namespace Prime___Michael_Dorfman
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
