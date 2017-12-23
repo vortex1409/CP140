eb93b01d91154309428884e0eb12c1162bbdb812
Michael <mvd1409@gmail.com>
2017-12-22 21:29:33 -0500

    Update bonus.cs

--- Prime_Bonus/bonus.cs
+++ Prime_Bonus/bonus.cs
@@ -7,21 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-/*
-    Project: Prime - Bonus Assignment (Q8 & Q9)
-    Description: Checks to see if number is prime and then prints then adds number to a listbox
-    Class: CP140 - OOP1
-    Name: Michael Dorfman
-    Date: 11/15/2016
-*/
+
 namespace Prime___Michael_Dorfman
 {
     public partial class Form1 : Form
     {
-        
+
+        bool IsPrime(int number)
+        {
+            int i;
+            for (i = 2; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    return true;
+                }
+            }
+            if (i == number)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int input = 0;
+            bool checkvar;
+
+            do
+            {
+                checkvar = IsPrime(input);
+                if (checkvar == true)
+                {
+                    PrimeOut.Items.Add(input);
+                }
+                else if (checkvar == false)
+                {
+                    PrimeOut.Items.Add("NOT PRIME");
+                }
+                input++;
+            } while (input <= 100);
         }
     }
 }
